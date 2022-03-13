9f98e86ac8d286d50003ae2cbe694133b4fd87e0
Leandro Lima <llimas@outlook.com>
2022-03-13 15:31:15 -0300

    removing unnecessary code

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"flag"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -14,66 +12,6 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
-
-func parseFlags() string {
-	strToSearch := flag.String("s", "", "Use -s stringtosearch")
-	flag.Parse()
-	return *strToSearch
-}
-
-func getLogs() {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-
-	if err != nil {
-		panic(err)
-	}
-
-	stringToSearch := parseFlags()
-	var oldLogContent string
-
-	for true {
-		reader, err := cli.ContainerLogs(context.Background(), "dcbd8eb49e", types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		defer reader.Close()
-
-		logContent, err := io.ReadAll(reader)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		logContentIsSame := strings.Compare(oldLogContent, string(logContent))
-
-		if logContentIsSame == -1 {
-			logContentArr := strings.Split(string(logContent), "\n")
-
-			for _, v := range logContentArr {
-				contains := strings.Contains(v, stringToSearch)
-				if contains {
-					fmt.Println(v)
-				}
-			}
-
-			containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for _, container := range containers {
-				fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-			}
-
-			oldLogContent = string(logContent)
-		}
-
-	}
-
-}
 
 type Container struct {
 	Name  string
